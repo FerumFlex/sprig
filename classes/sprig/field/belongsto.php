d9c84d0d4adcbe9f99a6423ec91801ae3ffbffe2
ferumflex <ferumflex@gmail.com>
2009-12-25 17:28:18 +0200

    add options --- when field could be empty

--- classes/sprig/field/belongsto.php
+++ classes/sprig/field/belongsto.php
@@ -9,7 +9,10 @@ class Sprig_Field_BelongsTo extends Sprig_Field_ForeignKey {
 		$model = Sprig::factory($this->model);
 
 		$choices = $model->select_list($model->pk());
-
+		
+		if ($this->empty)
+			$choices = arr::merge(array(NULL => '----'), $choices);
+		
 		return Form::select($name, $choices, $this->verbose($value), $attr);
 	}
 
