47d6fc0eee2173d5351fa8a077fdddfe5a4d9a5c
ferumflex <ferumflex@gmail.com>
2010-07-09 17:31:58 +0300

    uploaad

--- classes/sprig/field/upload.php
+++ classes/sprig/field/upload.php
@@ -110,8 +110,9 @@ class Sprig_Field_Upload extends Sprig_Field_Char {
 				$array->error('file', 'valid');
 			} else {
 				$this->delete($this->object->original($input));
+				$filename = filter::filename($file['name']);
 				
-				$array[$input] = basename(Upload::save($file, NULL, $this->base_dir.$this->path));
+				$array[$input] = basename(Upload::save($file, $filename, $this->base_dir.$this->path));
 			}
 		}
 		else
